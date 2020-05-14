ec0f1c623d3be5b623bb4589fee3cb0acfc2d94b
RevivaI <65326814+RevivaI@users.noreply.github.com>
2020-05-14 03:03:39 -0400

    Update funciones Agr.sql

--- funciones Agr.sql
+++ funciones Agr.sql
@@ -4,7 +4,7 @@ AS
 BEGIN
   IF EXISTS (SELECT * FROM Cliente WHERE nombre_instagram = @NombIg) 
     IF EXISTS (SELECT * FROM Pedido WHERE codigo_pedido = @CodPed) 
-	  IF EXISTS (SELECT * FROM Cliente_Pedido WHERE codigo_pedido = @CodPed AND nombre_instagram = @NombIg) --identifica si ya existe el pedido para el cliente
+	  IF EXISTS (SELECT * FROM Cliente_Pedido WHERE codigo_pedido = @CodPed AND nombre_instagram = @NombIg) 
 	      return -1;
       ELSE
 	      INSERT INTO Cliente_Pedido VALUES(@NombIg,@CodPed);
